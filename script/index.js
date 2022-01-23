267329e8b1bbe5abb84ad9178e7342ef0d17a048
xireiki <xiayinlily@outlook.com>
2022-01-23 16:23:09 +0800

    添加了一些设置

--- script/index.js
+++ script/index.js
@@ -40,9 +40,36 @@ function clearBackground(){
     alert("无需清除.");
   }
 }
+function setting(){
+  //获取或者设置设置信息
+  var info = {"bookmark": true, "bg": true, "bgOptimize": true};
+  if(localStorage.getItem("settingInfo")){
+    Info = localStorage.getItem("settingInfo");
+    info = $.parseJSON(Info);
+  } else {
+    Info = JSON.stringify(info);
+    localStorage.setItem("settingInfo", Info);
+  }
+  //执行设置
+  if(localStorage.getItem("backgroundImage") && info["bg"]){
+    $("html").css("background-image", "url(" + localStorage.getItem("backgroundImage") + ")");
+  } else if(info["bg"] && !localStorage.getItem("backgroundImage")){
+    $("html").addClass("bg");
+  }
+  if(info["bgOptimize"]){
+    $("html").css("background-position", "center 0");
+  }
+  //在设置中显示设置
+  for (name in info){
+    $("input[name=\"" + name + "\"]").prop("checked", info[name]);
+  }
+  //监听设置选项变化
+  $("input[type=\"checkbox\"]").click(function(){
+    info[this.name] = $(this).prop("checked");
+    Info = JSON.stringify(info);
+    localStorage.setItem("settingInfo", Info);
+  });
+}
 (function(){
   welcome();
-  if(localStorage.getItem("backgroundImage")){
-    $("html").css("background-image", "url(" + localStorage.getItem("backgroundImage") + ")");
-  }
 })();
